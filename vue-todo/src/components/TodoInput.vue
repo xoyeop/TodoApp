d0f9f24d49dc327b952ac9e2df4ecbe4fb11d940
xoyeop <bbusso90@gmail.com>
2019-01-11 02:11:06 +0900

    Modify TodoInput

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,15 +1,38 @@
 <template>
-    <div>
-        input
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem">
+        <button v-on:click="addTodo">ADD</button>
+        <span class="addContainer"></span>
     </div>
 </template>
 
 <script>
 export default {
-
+    data: function() {
+        return {
+            newTodoItem: ""
+        }
+    },
+    methods: {
+        addTodo: function() {
+            // eslint-disable-next-line
+            // console.log(this.newTodoItem);
+            // 저장하는 로직
+            localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            this.clearInput();
+        },
+        clearInput: function() {
+            this.newTodoItem = '';
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scopped>
+input :focus {
+    outline: none;
+}
+.inputBox {
+    background: 
+}
 </style>
