a4ca534ca4a85d42f25b3ac83181cbee2b647e17
xoyeop <bbusso90@gmail.com>
2019-01-11 18:06:21 +0900

    Implement all the basic components for Todo App

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="inputBox shadow">
-        <input type="text" v-model="newTodoItem">
-        <button v-on:click="addTodo">ADD</button>
-        <span class="addContainer"></span>
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+        <!--<button v-on:click="addTodo">ADD</button>-->
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="fas fa-plus addBtn"></i>
+        </span>
     </div>
 </template>
 
@@ -18,8 +20,11 @@ export default {
             // eslint-disable-next-line
             // console.log(this.newTodoItem);
             // 저장하는 로직
-            localStorage.setItem(this.newTodoItem, this.newTodoItem);
-            this.clearInput();
+            if(this.newTodoItem != '') {
+                var obj = {completed: false, item: this.newTodoItem};
+                localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+                this.clearInput();
+            }
         },
         clearInput: function() {
             this.newTodoItem = '';
@@ -29,8 +34,28 @@ export default {
 </script>
 
 <style scopped>
-input :focus {
+input:focus {
     outline: none;
 }
-
+.inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+.inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+}
+.addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+.addBtn {
+    color: white;
+    vertical-align: middle;
+}
 </style>
