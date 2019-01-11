2c9e7a5b45ca7ea6f2a8491f2fd98b286c55c98d
xoyeop <bbusso90@gmail.com>
2019-01-11 21:09:53 +0900

    TodoApp refactoring

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -21,8 +21,7 @@ export default {
             // console.log(this.newTodoItem);
             // 저장하는 로직
             if(this.newTodoItem != '') {
-                var obj = {completed: false, item: this.newTodoItem};
-                localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+                this.$emit('addTodoItem', this.newTodoItem);
                 this.clearInput();
             }
         },
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <ul>
-            <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+            <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
                 <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}"
                     v-on:click="toggleComplete(todoItem, index)"></i>
                 <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
@@ -15,33 +15,13 @@
 
 <script>
 export default {
-    data: function() {
-        return {
-            todoItems: []
-        }
-    },
+    props: ['propsdata'],
     methods: {
         removeTodo: function(todoItem, index) {
-            // console.log(todoItem, index);
-            localStorage.removeItem(todoItem);
-            this.todoItems.splice(index, 1);
+            this.$emit('removeItem', todoItem, index);
         },
         toggleComplete: function(todoItem, index) {
-            todoItem.completed = !todoItem.completed; // todoItem의 completed를 toggle
-            localStorage.removeItem(todoItem.item);   // localStorage는 update 기능이 없어서 remove->set
-            localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
-        }
-    },
-    created: function() {
-        if (localStorage.length > 0) {
-            for (var i = 0; i < localStorage.length; i++) {
-                if (localStorage.key(i) != 'loglevel:webpack-dev-server') {
-                    this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-                    // this.todoItems.push(localStorage.key(i));
-                    // console.log(localStorage.key(i));
-                }
-                
-            }
+            this.$emit('toggleItem', todoItem, index);
         }
     }
 }
